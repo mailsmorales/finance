168eedf2b223bd22b1ee4aea04fe2f23c24f33e4
mukhamed-Ali <mailsmorales21@mail.ru>
2022-06-27 02:27:32 +0600

    create file navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Home, Login, Signup } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>Мои финансы</li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
